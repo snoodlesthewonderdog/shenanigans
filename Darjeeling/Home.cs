35dc71755123d718c5e050ff064646a09580ee90
unknown-user <unknown-user@fe80::491f:5e9a:8d6b:28a0%17>
2015-04-30 17:31:43 -0400

    New Activities

--- Darjeeling/Home.cs
+++ Darjeeling/Home.cs
@@ -9,8 +9,10 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 
+
 namespace Darjeeling
 {
+	
 	[Activity (Label = "PreFloat", MainLauncher=true, Icon="@drawable/icon")]			
 	public class Home : Activity
 	{
@@ -19,6 +21,37 @@ namespace Darjeeling
 			base.OnCreate (bundle);
 			SetContentView (Resource.Layout.Home);
 			// Create your application here
+			View view = new View(this);
+			Button StartChecking = FindViewById<Button>(Resource.Id.btnStartChecking);
+			StartChecking.Click += delegate {
+				onClickStartChecking (view);
+			};
+
+			Button History = FindViewById<Button> (Resource.Id.btnHistory);
+			History.Click += delegate(object sender, EventArgs e) {
+				onClickHistory (view);
+			};
+
+			Button ManageLists = FindViewById<Button> (Resource.Id.btnManageLists);
+			ManageLists.Click += delegate(object sender, EventArgs e) {
+				onClickManageLists(view);
+			};
+		}
+		public void onClickStartChecking (View view){
+
+			Intent intent = new Intent(this, typeof(StartChecking));
+			StartActivity(intent);
+		}
+
+		public void onClickManageLists (View view){
+			Intent intent = new Intent (this, typeof(ManageLists));
+			StartActivity (intent);
+		}
+
+		public void onClickHistory (View view){
+			Intent intent = new Intent (this, typeof(History));
+			StartActivity (intent);
+
 		}
 	}
 }
